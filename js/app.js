82797cf1a8a90353f632c5956869804f1e83a123
Nadia Bahrami <nadia.bahrami@gmail.com>
2015-12-18 10:50:02 -0800

    Post lint clean

--- js/app.js
+++ js/app.js
@@ -1,40 +1,40 @@
-var products = ["bag.jpg", "banana.jpg","boots.jpg","chair.jpg","cthulhu.jpg","dragon.jpg","pen.jpg", "scissors.jpg", "shark.jpg", "sweep.jpg", "unicorn.jpg", "usb.gif", "water_can.jpg","wine_glass.jpg"];
+var products = ['bag.jpg', 'banana.jpg','boots.jpg','chair.jpg','cthulhu.jpg','dragon.jpg','pen.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.jpg', 'unicorn.jpg', 'usb.gif', 'water_can.jpg','wine_glass.jpg'];
 var barData = {
-	labels : [],
-	datasets : [
-		{
-			fillColor : "#48A497",
-			strokeColor : "#48A4D1",
-			data : []
-		},
-		{
-			fillColor : "rgba(73,188,170,0.4)",
-			strokeColor : "rgba(72,174,209,0.4)",
-			data : []
-		}
-	]
+  labels:	[],
+  datasets : [
+    {
+      fillColor : '#48A497',
+      strokeColor : '#48A4D1',
+      data : []
+    },
+    {
+      fillColor : 'rgba(73,188,170,0.4)',
+      strokeColor : 'rgba(72,174,209,0.4)',
+      data : []
+    }
+  ]
 };
 var things =[];
-var produce = document.getElementById("resultTable");
-var produceChart = document.getElementById("filled");
+var produce = document.getElementById('resultTable');
+var produceChart = document.getElementById('filled');
 
 function pic (spot){
-  this.path = "img/"+ products[spot];
+  this.path = 'img/'+ products[spot];
   this.item = products[spot].slice(0,-4);
   this.tally = 0;
   this.views = 0;
   barData.labels.push(this.item);
-};
+}
 
 function create(){
   for (var i=0; i<products.length; i++){
     var thing = new pic(i);
     things.push(thing);
-  };
-};
+  }
+}
 
 create();
-console.log(things)
+console.log(things);
 
 var productRank = {
   totalClicks: 0,
@@ -43,10 +43,10 @@ var productRank = {
   pic3: null,
   barChart: null,
 
-  results: document.getElementById("press"),
-  picA: document.getElementById("picA"),
-  picB: document.getElementById("picB"),
-  picC: document.getElementById("picC"),
+  results: document.getElementById('press'),
+  picA: document.getElementById('picA'),
+  picB: document.getElementById('picB'),
+  picC: document.getElementById('picC'),
 
   getRandomIndex: function() {
     return Math.floor(Math.random() * products.length);
@@ -59,26 +59,26 @@ var productRank = {
 
     if(productRank.pic1 === productRank.pic2 || productRank.pic1 === productRank.pic3 || productRank.pic2 === productRank.pic3)
     {
-    productRank.selection();
-    console.log("redo");
+      productRank.selection();
+      console.log('redo');
     }else{
       productRank.pic1.views += 1
       productRank.pic2.views += 1
       productRank.pic3.views += 1
     }
-     productRank.picA.src = productRank.pic1.path;
-     productRank.picA.id = productRank.pic1.item;
-     productRank.picB.src = productRank.pic2.path;
-     productRank.picB.id = productRank.pic2.item;
-     productRank.picC.src = productRank.pic3.path;
-     productRank.picC.id = productRank.pic3.item;
+    productRank.picA.src = productRank.pic1.path;
+    productRank.picA.id = productRank.pic1.item;
+    productRank.picB.src = productRank.pic2.path;
+    productRank.picB.id = productRank.pic2.item;
+    productRank.picC.src = productRank.pic3.path;
+    productRank.picC.id = productRank.pic3.item;
   },
 
   showResults: function(){
     if (productRank.totalClicks % 15 === 0){
-    productRank.results.hidden = false;
+      productRank.results.hidden = false;
     }else{
-    productRank.results.hidden = true;
+      productRank.results.hidden = true;
       produceChart.hidden = true;
     }
   }
@@ -111,27 +111,28 @@ productRank.picC.addEventListener('click', function(){
   productRank.selection();
 });
 
-var income = document.getElementById("filled").getContext("2d");
+var income = document.getElementById('filled').getContext('2d');
 function createChart(){
   produceChart.hidden = false;
   for(var i=0; i<things.length; i++){
     barData.datasets[0].data[i] =things[i].tally;
     barData.datasets[1].data[i] = things[i].views;
-  };
-    if(localStorage.tallies){
-      var bringTallies = JSON.parse(localStorage.tallies);
-      var bringViews = JSON.parse(localStorage.Views);
-      for(var i=0; i<things.length; i++){
-        barData.datasets[1].data[i] += bringViews[i];
-        barData.datasets[0].data[i] += bringTallies[i];
-      };
-      localStorage.setItem("tallies",  JSON.stringify(barData.datasets[0].data));
-      localStorage.setItem("Views", JSON.stringify(barData.datasets[1].data));
-    }else{
-      localStorage.setItem("tallies", JSON.stringify(barData.datasets[0].data));
-      localStorage.setItem("Views", JSON.stringify(barData.datasets[1].data));
   }
-  new Chart(income).Bar(barData);
+  if(localStorage.tallies){
+    var bringTallies = JSON.parse(localStorage.tallies);
+    var bringViews = JSON.parse(localStorage.Views);
+    for(var i=0; i<things.length; i++){
+      barData.datasets[1].data[i] += bringViews[i];
+      barData.datasets[0].data[i] += bringTallies[i];
+    }
+    localStorage.setItem('tallies',
+    JSON.stringify(barData.datasets[0].data));
+    localStorage.setItem('Views', JSON.stringify(barData.datasets[1].data));
+  }else{
+    localStorage.setItem('tallies', JSON.stringify(barData.datasets[0].data));
+    localStorage.setItem('Views', JSON.stringify(barData.datasets[1].data)
+
+    newChart(income).Bar(barData);
 }
 
 productRank.results.addEventListener('click', function(event){
